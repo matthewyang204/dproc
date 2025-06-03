9512e4b7166841c70eb45dafb4d7d2e871813983
Matthew Yang <matthew.yang204@gmail.com>
2025-06-03 16:59:00 +0800

    Finish writing mean function

--- src/round.rs
+++ src/round.rs
@@ -1,4 +1,14 @@
+// Load modules required for functions here
+mod enumerate;
+
+// Use functions from these modules
+use enumerate::sum;
+
 fn mean(data: &[f64]) -> f64 {
-	let mut toProcess = data.to_vec();
-	println!(toProcess)
+	if data.is_empty() {
+		eprintln!("WARNING: Your data is empty, so your value is also empty.");
+		return 0.0;
+	}
+	let sum: f64 = sum(data);
+	sum / data.len() as f64
 }
