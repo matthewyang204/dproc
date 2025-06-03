3bf6c2e0c2d8b3f887bf945264663c73a7568304
Matthew Yang <matthew.yang204@gmail.com>
2025-06-03 18:13:44 +0800

    Load enumerate module correctly

--- src/round.rs
+++ src/round.rs
@@ -1,10 +1,6 @@
-// Load modules required for functions here
-mod enumerate;
+// Load toplevel modules properly; this is a submodule
+use crate::enumerate::sum;
 
-// Use functions from these modules
-use enumerate::sum;
-
-fn mean(data: &[f64]) -> f64 {
 pub fn mean(data: &[f64]) -> f64 {
 	if data.is_empty() {
 		eprintln!("WARNING: Your data is empty, so your value is also empty.");
