253dab0ae61b39d418d1b4f7adefbd2b65bb3edd
Matthew Yang <matthew.yang204@gmail.com>
2025-06-07 22:02:20 +0800

    Add a partially implemented median function

--- src/round.rs
+++ src/round.rs
@@ -9,3 +9,11 @@ pub fn mean(data: &[f64]) -> f64 {
 	let sum: f64 = sum(data);
 	sum / data.len() as f64
 }
+
+pub fn median(data: &[f64]) -> f64 {
+	if data.is_empty() {
+		eprintln!("WARNING: Your data is empty, so your value is also empty.");
+		return 0.0;
+	}
+	println!("Unimplemented STUB");
+}
