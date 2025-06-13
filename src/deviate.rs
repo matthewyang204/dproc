4123a8f277b154f25e11acb9d7f63560eb31fdd1
Matthew Yang <matthew.yang204@gmail.com>
2025-06-13 18:29:05 +0800

    Start writing variance and stub it

--- src/deviate.rs
+++ src/deviate.rs
@@ -1,5 +1,6 @@
 // Load toplevel modules properly; this is a submodule
 use crate::sort::sort;
+use crate::round::mean;
 
 pub fn range(data: &[f64]) -> f64 {
 	let sortedData = sort(data);
@@ -7,4 +8,11 @@ pub fn range(data: &[f64]) -> f64 {
 	let min = sortedData[0];
 	let range = max - min;
 	return range;
+}
+
+pub fn variance(data: &[f64]) -> f64 {
+	let mean = mean(data);
+	println!("Unimplemented STUB");
+	println!("Returning 1.0 to handle STUB");
+	return 1.0;
 }
