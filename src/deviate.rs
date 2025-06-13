853c26ed5312ee160cfd518582c3b8d7270a1bbe
Matthew Yang <matthew.yang204@gmail.com>
2025-06-13 20:42:54 +0800

    Add variance function

--- src/deviate.rs
+++ src/deviate.rs
@@ -1,6 +1,8 @@
 // Load toplevel modules properly; this is a submodule
 use crate::sort::sort;
 use crate::round::mean;
+use crate::coreFuncs::input;
+use crate::enumerate::sum;
 
 pub fn range(data: &[f64]) -> f64 {
 	let sortedData = sort(data);
@@ -11,7 +13,28 @@ pub fn range(data: &[f64]) -> f64 {
 }
 
 pub fn variance(data: &[f64]) -> f64 {
-	let mean = mean(data);
+	let meanData = mean(data);
+	let mut diff = vec![];
+	let mut tempStore: f64 = 0.0;
+	for value in data {
+		tempStore = value - meanData;
+		diff.push(tempStore);
+	}
+	let mut diffSq = vec![];
+	for value in diff {
+		tempStore = value.powi(2);
+		diffSq.push(tempStore);
+	}
+	let mut varianceVal: f64 = 0.0;
+	let response1 = input("Is this a S)ample or a P)opulation? ");
+	if response1.to_lowercase() == "p" {
+		let sum = sum(&diffSq);
+		varianceVal = sum / diffSq.len() as f64;
+	} else if response1.to_lowercase() == "s" {
+		let sum = sum(&diffSq);
+		varianceVal = sum / (diffSq.len() as f64 - 1.0);
+	}
+	return varianceVal;
 	println!("Unimplemented STUB");
 	println!("Returning 1.0 to handle STUB");
 	return 1.0;
