24cdbdab4ed4397516df20d18768f3ccd83e6a6d
Matthew Yang <matthew.yang204@gmail.com>
2025-07-17 18:52:30 -0700

    Update help infos

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,7 @@ fn help() {
 	println!("");
 	println!("  When SUBCMD1 is solve:");
 	println!("    quadratic-single {{a}} {{b}} {{c}}   solve a quadratic equation with the quadratic formula, where a, b, and c are the coefficients of the quadratic equation ax^2 + bx + c = 0. Note that you may have to convert your equation; this does not accept != 0 on the other side of the equation.");
+	println!("    linear-dual {{a1}} {{b1}} {{c1}} {{a2}} {{b2}} {{c2}}   solve a system of two linear equations with the substitution method, where a1, b1, c1 are the coefficients of the first linear equation a1x + b1y + c1 = 0 and a2, b2, c2 are the coefficients of the second linear equation a2x + b2y + c2 = 0. Note that this is standard form and you may have to convert your equations to it.");
 	println!("");
 	println!("Place your data, values separated by spaces, in the place of {{yourdata}}.");
 }
