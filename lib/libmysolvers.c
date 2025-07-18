a289f5511689fbae201fea8ebce0f6cc8f473f16
Matthew Yang <matthew.yang204@gmail.com>
2025-07-17 17:18:42 -0700

    Upgrade libmysolvers

--- lib/libmysolvers.c
+++ lib/libmysolvers.c
@@ -1,3 +1,7 @@
+// Copyright 2025 Matthew Yang
+// This file is part of libmysolvers.
+// libmysolvers is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
 #include "libmysolvers.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,4 +37,26 @@ double quadratic_single_pos_solver(double a, double b, double c) {
 
     double x = (-b + sqrt(discriminant)) / (2 * a);
     return x;
+}
+ // This uses cramer's rule to solve a system of linear equations
+ // This can only solve a system of two linear equations with two variables
+double linear_solver(double a1, double b1, double c1, double a2, double b2, double c2) {
+    double x;
+    double y;
+
+    double d = a1 * b2 - a2 * b1;
+    x = (c1 * b2 - c2 * b1) / d;
+    y = (a1 * c2 - a2 * c1) / d;
+
+    if (d == 0) {
+        if (x == 0 && y == 0) {
+            fprintf(stderr, "ERROR: The system has infinitely many solutions.\n");
+            exit(EXIT_FAILURE);
+        } else {
+            fprintf(stderr, "ERROR: The system has no solution.\n");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        return x, y;
+    }
 }
--- lib/libmysolvers.h
+++ lib/libmysolvers.h
@@ -1,7 +1,12 @@
+// Copyright 2025 Matthew Yang
+// This file is part of libmysolvers.
+// libmysolvers is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
 #ifndef LIBMYSOLVERS_H
 #define LIBMYSOLVERS_H
 
 double quadratic_single_neg_solver(double a, double b, double c);
 double quadratic_single_pos_solver(double a, double b, double c);
+double linear_solver(double a1, double b1, double c1, double a2, double b2, double c2);
 
 #endif
