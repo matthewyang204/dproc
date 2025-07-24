8906bf3188cbbcabbae9c18455c9bb3c52867bd5
Matthew Yang <matthew.yang204@gmail.com>
2025-07-24 06:56:11 -0700

    Declare data as new vec and then overwrite

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,7 @@ fn main() {
 		userError();
         }
 	
-	let data: Vec<f64>;
+	let mut data: Vec<f64> = Vec::new();
 	if args[3] == "stdin" || args[3] == "-" {
 		let stdin = io::stdin();
 		let mut lines: Vec<String> = Vec::new();
