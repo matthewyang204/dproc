077087508debd2307a13f4719764420223acae27
Matthew Yang <matthew.yang204@gmail.com>
2025-09-05 15:59:56 -0700

    Retrieve the temporary directory and some other variables

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,15 @@
 import tkinter as tk
+import platform
+import os
+import sys
+
+# Initalize some variables
+print("Retrieiving temporary directory...", end='')
+if platform.system() == "Windows":
+    print("Unimplemented STUB")
+else:
+    temp = os.path.join("/tmp")
+    print(temp)
 
 root = tk.Tk()
 root.title("dproc GUI")
