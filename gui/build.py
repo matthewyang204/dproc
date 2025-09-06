f0d6d5ef39a4a32538d5d92c1bb9822ae42d5d01
Matthew Yang <matthew.yang204@gmail.com>
2025-09-06 12:29:57 -0700

    No icon

--- gui/build.py
+++ gui/build.py
@@ -14,7 +14,7 @@ def run_pyinstaller():
             main_script,
             '-w',  # Makes it windowed
             '--name', 'dproc-gui',
-            '--icon=icon.ico',
+            # '--icon=icon.ico',
             '--onefile'  # Bundle into a single executable
         ]
 
