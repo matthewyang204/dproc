76826716ffbf5ab9f513ed7a6894f2d5e831d9a6
Matthew Yang <matthew.yang204@gmail.com>
2025-09-06 12:38:56 -0700

    Add configuration abilities to build.py

--- gui/build.py
+++ gui/build.py
@@ -3,20 +3,34 @@ import sys
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
+arguments = sys.argv
 
-def run_pyinstaller():
+def run_pyinstaller(config):
     try:
         main_script = os.path.join(script_dir, 'main.py')
 
         # PyInstaller command to build the executable
-        cmd = [
-            'pyinstaller',
-            main_script,
-            '-w',  # Makes it windowed
-            '--name', 'dproc-gui',
-            # '--icon=icon.ico',
-            '--onefile'  # Bundle into a single executable
-        ]
+        if config == "release":
+            cmd = [
+                'pyinstaller',
+                main_script,
+                '-w',  # Makes it windowed
+                '--name', 'dproc-gui',
+                # '--icon=icon.ico',
+                '--onefile'  # Bundle into a single executable
+            ]
+        elif config == "debug":
+            cmd = [
+                'pyinstaller',
+                main_script,
+                # '-w',  # Makes it windowed
+                '--name', 'dproc-gui',
+                # '--icon=icon.ico',
+                '--onefile'  # Bundle into a single executable
+            ]
+        else:
+            print("ERROR: Invalid build configuration. Use 'release' or 'debug'.")
+            sys.exit(1)
 
         # Run PyInstaller
         subprocess.check_call(cmd)
@@ -27,4 +41,10 @@ def run_pyinstaller():
 
 
 if __name__ == '__main__':
-    run_pyinstaller()
+    if "--release" in arguments:
+        run_pyinstaller("release")
+    elif "--debug" in arguments:
+        run_pyinstaller("debug")
+    else:
+        print("Using default 'release' configuration.")
+        run_pyinstaller("release")
