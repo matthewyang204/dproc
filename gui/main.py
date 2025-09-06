38f52f8bd7e6ace6bc0d51dcf31fea0280a28471
Matthew Yang <matthew.yang204@gmail.com>
2025-09-05 17:31:11 -0700

    Add run class with new function to process data that will be linked to GUI

--- gui/main.py
+++ gui/main.py
@@ -37,8 +37,12 @@ scrollbar = tk.Scrollbar(text_frame, orient="vertical", command=text_area.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 text_area.config(yscrollcommand=scrollbar.set)
 
-def run_dproc(data):
-    print("Unimplemented STUB")
+class run():
+    def run_dproc(data):
+        print("Unimplemented STUB")
+    
+    def process_data(event=None):
+        print("Unimplemented STUB")
 
 text_area.pack(fill=tk.BOTH, expand=tk.YES, side=tk.LEFT)
 
