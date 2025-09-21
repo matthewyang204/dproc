7e69658a73af00afef18d55715b2e7be847a0206
Matthew Yang <matthew.yang204@gmail.com>
2025-09-21 16:39:53 -0700

    I suppose we can call this one stable

--- gui/main.py
+++ gui/main.py
@@ -44,7 +44,7 @@ print(dproc)
 print("Checking version info...")
 dprocVersionInfo = subprocess.run([dproc, '--version'], capture_output=True, text=True)
 dprocHelpInfo = subprocess.run([dproc, '--help'], capture_output=True, text=True)
-guiVersionInfo = """dproc GUI, rolling release
+guiVersionInfo = """dproc GUI, rolling release, build 2025.9.21
 (C) 2025 Matthew Yang"""
 versionInfo = f"""dproc GUI:
 {guiVersionInfo}
