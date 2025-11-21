1dbc29fdc2bb209f8ac35432fcc6aa5152d9ff0f
Matthew Yang <matthew.yang204@gmail.com>
2025-11-21 07:01:35 -0800

    Fix alignment for some other subcommands

--- src/main.rs
+++ src/main.rs
@@ -129,8 +129,8 @@ fn help() {
 	println!("    factorial      get the factorial of each number in the dataset");
 	println!("");
 	println!("  When SUBCMD1 is solve:");
-	println!("    quadratic-single {{a}} {{b}} {{c}}   solve a quadratic equation with the quadratic formula, where a, b, and c are the coefficients of the quadratic equation ax^2 + bx + c = 0. Note that you may have to convert your equation; this does not accept != 0 on the other side of the equation.");
-	println!("    linear-dual {{a1}} {{b1}} {{c1}} {{a2}} {{b2}} {{c2}}   solve a system of two linear equations with the substitution method, where a1, b1, c1 are the coefficients of the first linear equation a1x + b1y = c1 and a2, b2, c2 are the coefficients of the second linear equation a2x + b2y = c2. Note that this is standard form and you may have to convert your equations to it.");
+	println!("    quadratic-single {{a}} {{b}} {{c}}                            solve a quadratic equation with the quadratic formula, where a, b, and c are the coefficients of the quadratic equation ax^2 + bx + c = 0. Note that you may have to convert your equation; this does not accept != 0 on the other side of the equation.");
+	println!("    linear-dual {{a1}} {{b1}} {{c1}} {{a2}} {{b2}} {{c2}}         solve a system of two linear equations with the substitution method, where a1, b1, c1 are the coefficients of the first linear equation a1x + b1y = c1 and a2, b2, c2 are the coefficients of the second linear equation a2x + b2y = c2. Note that this is standard form and you may have to convert your equations to it.");
 	println!("    triangle-centroid {{x1}} {{y1}} {{x2}} {{y2}} {{x3}} {{y3}}   get the centroid of triangle A(x1, y1) B(x2, y2) C(x3, y3)");
 	println!("");
 	println!("  When SUBCMD1 is electrical:");
