43ce5610088a8e653ab7fa78fbb66d02c1ce2faf
Matthew Yang <matthew.yang204@gmail.com>
2025-12-10 20:52:21 -0800

    Move cc to build-dependencies & set build.rs to compile and link the C code without Make's help
    This'll probably be all it takes to get it to work as a crate

--- build.rs
+++ build.rs
@@ -1,3 +1,17 @@
+use cc;
+
+fn build() {
+    cc::Build::new()
+        .file("lib/libcharge.c")
+        .include("lib")
+        .compile("charge");
+
+    cc::Build::new()
+        .file("lib/libmysolvers.c")
+        .include("lib")
+        .compile("mysolvers");
+}
+
 fn link() {
     println!("cargo:rustc-link-search=native=obj");
     println!("cargo:rustc-link-lib=static=mysolvers");
@@ -6,5 +20,6 @@ fn link() {
 }
 
 fn main() {
+    build();
     link();
 }
