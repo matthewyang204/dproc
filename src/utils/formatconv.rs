8970c91512df235a08ce1dd11ccb8e254733bb01
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-03-19 18:51:43 +0000

    Add space-delimited functions

--- src/utils/formatconv.rs
+++ src/utils/formatconv.rs
@@ -29,6 +29,27 @@ fn write_csv_column(file_path: &str, column_number: &f64, data: &[String]) -> Re
     Ok(())
 }
 
+fn read_space_delimited_values(file_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut values = Vec::new();
+    let file = File::open(file_path)?;
+    let reader = std::io::BufReader::new(file);
+    for line in reader.lines(){
+        match line {
+            Ok(content) => values.push(content),
+            Err(e) => eprintln!("ERROR: Error reading line from stdin: {}", e),
+        }
+    }
+    Ok(values)
+}
+
+fn write_space_delimited_values(file_path: &str, values: &[String]) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::create(file_path)?;
+    let line = values.join(" ");
+    use std::io::Write;
+    file.write_all(line.as_bytes())?;
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
 }
