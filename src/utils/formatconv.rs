ed12cd1388e52455e857b22f98ac5f64630e3999
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-03-19 18:44:41 +0000

    Use column number rather than name

--- src/utils/formatconv.rs
+++ src/utils/formatconv.rs
@@ -2,12 +2,12 @@ use csv;
 use std::error::Error;
 use std::fs::File;
 
-fn read_csv_column(file_path: &str, column_name: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+fn read_csv_column(file_path: &str, column_number: &f64) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_path(file_path)?;
     let mut column_data = Vec::new();
     let headers = rdr.headers()?.clone();
     let results = rdr.records();
-    let col_index = headers.iter().position(|h| h == column_name).unwrap();
+    let col_index = *column_number as usize;
 
     for result in results {
         if let Some(value) = result?.get(col_index) {
