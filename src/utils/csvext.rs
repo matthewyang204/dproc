67cbfe2905c9985c1142f69ca1ffb3d129ace357
Matthew Yang <matthew.yang204@gmail.com>
2026-03-26 15:01:20 -0700

    Move stuff to handle space-delimited values to a separate module

--- src/utils/csvext.rs
+++ src/utils/csvext.rs
@@ -69,24 +69,3 @@ pub fn write_csv_row(file_path: &str, row_number: &f64, data: &[String]) -> Resu
     wtr.flush()?;
     Ok(())
 }
-
-pub fn read_space_delimited_values(file_path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let mut values = Vec::new();
-    let file = File::open(file_path)?;
-    let reader = std::io::BufReader::new(file);
-    for line in reader.lines(){
-        match line {
-            Ok(content) => values.push(content),
-            Err(e) => eprintln!("ERROR: Error reading line from stdin: {}", e),
-        }
-    }
-    Ok(values)
-}
-
-pub fn write_space_delimited_values(file_path: &str, values: &[String]) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::create(file_path)?;
-    let line = values.join(" ");
-    use std::io::Write;
-    file.write_all(line.as_bytes())?;
-    Ok(())
-}
