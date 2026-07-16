fce869a9f3b6ddcf5dffb2054b7379381209de0e
Matthew Yang <matthew.yang204@gmail.com>
2026-07-16 11:16:19 -0700

    Add some nice conveniences at the bottom

--- src/humans.rs
+++ src/humans.rs
@@ -1,4 +1,4 @@
-pub fn format_float(value: String) -> String {
+pub fn trim_stringfloat(value: String) -> String {
     if let Some(_) = value.find('.') {
         value
             .trim_end_matches('0')
@@ -24,7 +24,7 @@ pub fn get_max_precision_from_list(values: &[f64]) -> usize {
     let mut max_precision = 0;
 
     for &value in values {
-        let formatted = format_float(value);
+        let formatted = trim_stringfloat(value.to_string());
         if let Some(pos) = formatted.find('.') {
             let precision = formatted.len() - pos - 1;
             if precision > max_precision {
@@ -34,4 +34,22 @@ pub fn get_max_precision_from_list(values: &[f64]) -> usize {
     }
 
     max_precision
+}
+
+// Quick function to do everything here
+pub fn format_float_listbased(list: &[f64], value: f64) -> String {
+    let max_precision = get_max_precision_from_list(list);
+    let cleaned_value = format_float_with_precision(value, max_precision);
+    trim_stringfloat(cleaned_value)
+}
+
+// Or do this instead if you have a list
+pub fn format_floatlist(list: &[f64]) -> Vec<String> {
+    let max_precision = get_max_precision_from_list(list);
+    list.iter()
+        .map(|&value| {
+            let cleaned_value = format_float_with_precision(value, max_precision);
+            trim_stringfloat(cleaned_value)
+        })
+        .collect()
 }
